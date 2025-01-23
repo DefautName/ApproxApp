3accf5c3dc0e4506a32851a4d10999b658808a9c
Кузин Илья < @LAPTOP-5ST58774>
2025-01-23 23:21:06 +0300

    Добавьте файлы проекта.

---
+++ Апроксимация/Func.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+
+namespace Апроксимация
+{
+    internal class AppFunc
+    {
+        private double  x1,x2,x3,y1,y2,y3;
+        private double[] coefsFunc;
+
+        public double X1 { get { return x1; } }
+        public double X2 { get { return x2; } }
+        public double X3 { get { return x3; } }
+        public double Y1 { get { return y1; } }
+        public double Y2 { get { return y2; } }
+        public double Y3 { get { return y3; } }
+
+        public AppFunc(Point p1,Point p2,Point p3)
+        {
+            x1= p1.X;
+            y1 = p1.Y;
+
+            x2 = p2.X;
+            y2 = p2.Y;
+
+            x3 = p3.X;
+            y3= p3.Y;
+
+            SetFunc();
+        }
+        private void SetFunc()
+        {
+            coefsFunc = new double[3];
+
+            double a1, a2, a3, d1, d2, d3;
+
+            d1 = x1;
+            d2 = x2;
+            d3 = x3;
+
+            a1 = y1 / (d1 - d2) / (d1 - d3);
+            a2 = y2 / (d2 - d1) / (d2 - d3);
+            a3 = y3 / (d3 - d1) / (d3 - d2);
+
+            coefsFunc[0] = a1 + a2 + a3;
+            coefsFunc[1] = (a1 * (d2 + d3) + a2 * (d1 + d3) + a3 * (d1 + d2)) * (-1);
+            coefsFunc[2] = a1 * d2 * d3 + a2 * d1 * d3 + a3 * d1 * d2;
+        }
+
+        public double[] GetFunc()
+        {
+            return coefsFunc;
+        }
+
+    
+        public Point GetExPoint()
+        {
+            Point rez = new Point();
+            rez.X = -coefsFunc[1] / (2 * coefsFunc[0]);
+            rez.Y = coefsFunc[0] * rez.X * rez.X + coefsFunc[1] * rez.X + coefsFunc[2];
+            return rez;
+        }
+        public bool WhereIsExPoint()
+        {
+            Point ex = GetExPoint();
+            if ((ex.X < x3 && ex.X > x1) ||(ex.X < x1 && ex.X > x3))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Апроксимация/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using ScottPlot;
+
+namespace Апроксимация
+{//  <package id="ScottPlot" version="4.1.64" targetFramework="net472" />
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AppFunc func;
+        double[] funMass;
+        private void GetSig(TextBlock Sig,TextBlock block,double fun)
+        {
+            if(fun<0)
+                Sig.Text = "";
+            else
+                Sig.Text = "+";
+            block.Text = fun.ToString().Replace(',', '.');
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            buffButt.IsEnabled = false;
+        }
+
+        private void ButtonCalc_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                Point p1 = new Point(double.Parse(X1.Text.Replace('.',',')), double.Parse(Y1.Text.Replace('.', ',')));
+                Point p2 = new Point(double.Parse(X2.Text.Replace('.', ',')), double.Parse(Y2.Text.Replace('.', ',')));
+                Point p3 = new Point(double.Parse(X3.Text.Replace('.', ',')), double.Parse(Y3.Text.Replace('.', ',')));
+                func = new AppFunc(p1, p2, p3);
+
+                funMass = func.GetFunc();
+                A.Text = Math.Round(funMass[0],4).ToString().Replace(',', '.');
+
+                GetSig(bSig, B, Math.Round(funMass[1],4));
+                GetSig(cSig,C, Math.Round(funMass[2],4));
+
+                Point exPoint = func.GetExPoint();
+
+                exX.Text=Math.Round(exPoint.X,4).ToString().Replace(',','.');
+                exY.Text= Math.Round(exPoint.Y,4).ToString().Replace(',', '.');
+                if (func.WhereIsExPoint())
+                    exPos.Text = "ВНУТРИ";
+                else
+                    exPos.Text = "ВНЕ";
+                double []valueX = new double[10];
+                double []valueY = new double[10];
+                int nOfStep = 10;
+                double step = (func.X3 - func.X3) / nOfStep;
+                for (int i = 0; i < nOfStep; i++)
+                    valueX[i]=func.X1 + step * i;
+                for (int i = 0; i < nOfStep; i++)
+                    valueY[i]=funMass[0] * valueX[i] * valueX[i] + funMass[1] * valueX[i] + funMass[2];
+
+                //FuncGraf.Render();
+                buffButt.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ButtonBuff_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string buff = $"{funMass[0].ToString().Replace(',', '.')} {funMass[1].ToString().Replace(',', '.')} {funMass[2].ToString().Replace(',', '.')}";
+                Clipboard.SetText(buff);
+                MessageBox.Show("Значения коэффициентов a,b,c функции скопированы в бувер обмена");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
